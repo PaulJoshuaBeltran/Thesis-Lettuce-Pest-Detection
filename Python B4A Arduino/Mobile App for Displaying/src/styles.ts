7c9c5c2ee57d5a8e1bfbcc855a0cf1ddc865031a
PaulJoshuaBeltran <60290308+PaulJoshuaBeltran@users.noreply.github.com>
2024-12-14 21:53:33 +0800

    Add files via upload

---
+++ Python B4A Arduino/Mobile App for Displaying/src/styles.ts
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  headerBackButton: {
+    marginLeft: 8,
+    marginRight: 11,
+    marginVertical: 10.5,
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
+
+export default styles;
